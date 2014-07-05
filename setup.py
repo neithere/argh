f5da5c3269712b921004e8763b662040c1ed2db8
Andy Mikhaylenko <neithere@gmail.com>
2014-07-05 18:06:38 +0200

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: User Interfaces',
